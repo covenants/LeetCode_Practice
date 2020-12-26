65040700d79f740fced789ebc22f7c7dd8f8d0f1
codeingreen <codeingreen@outlook.com>
2020-12-25 21:26:22 -0800

    Create 242. Valid Anagram.py
    https://leetcode.com/problems/valid-anagram/

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        # also can be done using Counter easily
+        d1, d2 = {}, {}
+        
+        for i in s:
+            d1[i] = d1.get(i, 0) + 1
+        
+        for j in t:
+            d2[j] = d2.get(j, 0) + 1
+        
+        # Check if same number of characters
+        if len(d1.keys()) != len(d2.keys()):
+            return False
+        
+        for key, value in d1.items():
+            if key in d1 and key in d2:
+                if d1[key] != d2[key]:
+                    return False
+            else:
+                return False
+            
+        return True
+    
