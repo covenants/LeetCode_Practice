b2f307c0bd35796fa860e4d589d7627d5085ad42
mmkhaque <50067081+mmkhaque@users.noreply.github.com>
2021-03-14 21:08:16 -0700

    Update 42. Trapping Rain Water.py

--- 42. Trapping Rain Water.py
+++ 42. Trapping Rain Water.py
@@ -33,3 +33,44 @@ class Solution:
             water += min(right_max[i], left_max[i]) - height[i]
         
         return water
+
+    # TC (O(n)) SC (O(N))
+    
+    '''
+    # Brute Force O(N)
+    
+    class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        i = 0
+        j = 1
+        water = 0
+        
+        i, j = 0, 0
+        
+        for i in range(0, len(height)):
+            left_max = 0
+            right_max = 0
+            
+            # Find the min border side on the left side
+            for j in range(i, -1, -1):
+                left_max = max(left_max, height[j])
+            
+            # Find the max border height on the right side
+            for j in range(i, len(height)):
+                right_max = max(right_max, height[j])
+
+            print(i, left_max, right_max)
+            # min of the two minus height of the bloack section
+            # This adds water for index i only
+            water += min(left_max, right_max) - height[i]
+        
+        print(water)
+        
+        return water
+   Complexity Analysis
+
+Time complexity: O(n^2)
+Space complexity: O(1)
+    
+    '''
