afc51a9f90df162ce0d87c904dde5c2191d61701
mmkhaque <50067081+mmkhaque@users.noreply.github.com>
2021-03-24 21:42:53 -0700

    Update 680. Valid Palindrome II.py

--- 680. Valid Palindrome II.py
+++ 680. Valid Palindrome II.py
@@ -1,3 +1,17 @@
+'''
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+
+Example 2:
+Input: "abca"
+Output: True
+
+Explanation: You could delete the character 'c'.
+'''
+
 ### BruteForce
 class Solution:
     def validPalindrome(self, s: str) -> bool:
