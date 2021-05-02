f07b3d480341332ebd946db739bc6660276a26fb
mmkhaque <50067081+mmkhaque@users.noreply.github.com>
2021-05-01 19:08:58 -0700

    official solution
    https://leetcode.com/problems/accounts-merge/

---
+++ 721. Accounts Merge.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def accountsMerge(self, accounts):
+        em_to_name = {}
+        graph = collections.defaultdict(set)
+        for acc in accounts:
+            name = acc[0]
+            for email in acc[1:]:
+                graph[acc[1]].add(email)
+                graph[email].add(acc[1])
+                em_to_name[email] = name
+
+        '''        
+        for k, v in graph.items():
+            print(k, ' => ', v)
+            
+        for k, v in em_to_name.items():
+            print(k, ' => ', v)
+        '''
+        
+        seen = set()
+        ans = []
+        for email in graph:
+            if email not in seen:
+                seen.add(email)
+                stack = [email]
+                component = []
+                while stack:
+                    node = stack.pop()
+                    component.append(node)
+                    for nei in graph[node]:
+                        if nei not in seen:
+                            seen.add(nei)
+                            stack.append(nei)
+                ans.append([em_to_name[email]] + sorted(component))
+        return ans
