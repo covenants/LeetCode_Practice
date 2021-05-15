41d10210b1cd72b21cd3779dfeec3e4108bcddd6
mmkhaque <50067081+mmkhaque@users.noreply.github.com>
2021-05-14 22:40:26 -0700

    Update 10. Regular Expression Matching.py

--- 10. Regular Expression Matching.py
+++ 10. Regular Expression Matching.py
@@ -1,5 +1,30 @@
 '''
 https://leetcode.com/problems/regular-expression-matching/
+
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+Example 3:
+
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+Example 4:
+
+Input: s = "aab", p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+Example 5:
+
+Input: s = "mississippi", p = "mis*is*p*."
+Output: false
 '''
 
 import re
