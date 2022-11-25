406d16c06bcf6db01e3ebcca75af53aab7fb1557
covenants <mmkhaque@gmail.com>
2022-11-25 11:05:56 -0800

    Added a test file

---
+++ test2.py
@@ -0,0 +1,14 @@
+def return_value():
+    a = 10
+    b = 20
+    print('\nTOtal of a and b are ', return_value())
+
+    return a+b
+    
+# write a function taht returns multiplicaiton of 3 values
+def return_multiplication():
+    a = 10
+    b = 20
+    c = 30
+
+    return a*b*c
